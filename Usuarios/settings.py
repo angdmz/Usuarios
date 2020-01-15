7039cb7ab12581d503b20518279209f7d79fcb9d
adorda <angdmz@gmail.com>
2020-01-15 15:51:11 -0300

    fix debug

--- Usuarios/settings.py
+++ Usuarios/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = os.getenv('SECRET_KEY','somesecretkey')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG', False)
+DEBUG = bool(os.getenv('DEBUG', False))
 
 ALLOWED_HOSTS = ['*']
 
